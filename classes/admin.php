548cbcafc4091ce38af1f5cfa2441c8532e326f8
MohamedHamouch <med10hamouch@gmail.com>
2025-01-17 09:32:56 +0100

    creating methods in admin class to manage tags categories users and content

--- classes/admin.php
+++ classes/admin.php
@@ -18,4 +18,113 @@ class Admin extends User
     $this->created_at = date('Y-m-d H:i:s');
   }
 
+  public function getAllUsers(PDO $db)
+  {
+    $query = "SELECT * FROM users";
+    $stmt = $db->query($query);
+    return $stmt->fetchAll();
+  }
+
+  public function activateUser(PDO $db, $user_id)
+  {
+    $query = "UPDATE users SET is_active = 1 WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+  }
+
+  public function suspendUser(PDO $db, $user_id)
+  {
+    $query = "UPDATE users SET is_suspended = 1 WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+  }
+
+  public function unsuspendUser(PDO $db, $user_id)
+  {
+    $query = "UPDATE users SET is_suspended = 0 WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+  }
+
+  public function deleteUser(PDO $db, $user_id)
+  {
+    $query = "DELETE FROM users WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+  }
+
+  public function addCategory(PDO $db, Category $category)
+  {
+    $categoryName = $category->getName();
+    $categoryDescription = $category->getDescription();
+
+    $query = "SELECT COUNT(*) FROM categories WHERE name = :name";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':name', $categoryName);
+    $stmt->execute();
+
+    $count = $stmt->fetchColumn();
+    if ($count > 0) {
+
+      return "Failed to add, '$categoryName' already exists.";
+    } else {
+
+      $query = "INSERT INTO categories (name, description) VALUES (:name, :description)";
+      $stmt = $db->prepare($query);
+      $stmt->bindParam(':name', $categoryName);
+      $stmt->bindParam(':description', $categoryDescription);
+      if ($stmt->execute()) {
+        return "'$categoryName' category added successfully.";
+      } else {
+        return "Failed to add '$categoryName' category.";
+      }
+    }
+  }
+
+  public function deleteCategory(PDO $db, $category_id)
+  {
+    $query = "DELETE FROM categories WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $category_id);
+    $stmt->execute();
+  }
+
+  public function addTag(PDO $db, Tag $tag)
+  {
+    $tagName = $tag->getName();
+
+    $query = "SELECT COUNT(*) FROM tags WHERE name = :name";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':name', $tagName);
+    $stmt->execute();
+
+    $count = $stmt->fetchColumn();
+    if ($count > 0) {
+
+      return "Failed to add, '$tagName' already exists.";
+    } else {
+
+      $query = "INSERT INTO tags (name) VALUES (:name)";
+      $stmt = $db->prepare($query);
+      $stmt->bindParam(':name', $tagName);
+
+      if ($stmt->execute()) {
+        return "'$tagName' tag added successfully.";
+      } else {
+        return "Failed to add '$tagName' tag.";
+      }
+    }
+  }
+
+  public function deleteTag(PDO $db, $tag_id)
+  {
+    $query = "DELETE FROM tags WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $tag_id);
+    $stmt->execute();
+  }
 }
