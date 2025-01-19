6b78c5f41897db40b1608910ba24a6374b6bda67
MohamedHamouch <med10hamouch@gmail.com>
2025-01-19 08:40:45 +0100

    defining method for both document and video course classes to update course and it's tags

--- classes/documentCourse.php
+++ classes/documentCourse.php
@@ -3,10 +3,94 @@ require_once 'course.php';
 
 class DocumentCourse extends Course
 {
-  public function __construct($id, $title, $description, $content, $category_id, $teacher_id, $created_at = null)
+  private $text_content;
+
+  public function __construct($id, $title = null, $description = null, $teacher_id = null, $image = null, $category_id = null, $tags = null, $text_content = null, $created_at = null)
   {
-    parent::__construct($id, $title, $description, $content, $category_id, $teacher_id, $created_at);
-    $this->type = 'document';
+    parent::__construct($id, $title, $description, $teacher_id, $image, $category_id, $tags, 'document', $created_at);
+
+    $this->text_content = $text_content;
+  }
+
+  public function getTextContent()
+  {
+    return $this->text_content;
+  }
+  public function setTextContent($text_content)
+  {
+    $this->text_content = $text_content;
+  }
+
+  public function loadCourse(PDO $db)
+  {
+    $query = "SELECT * FROM courses WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $this->id);
+    $stmt->execute();
+
+    $course = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($course) {
+      $this->title = $course['title'];
+      $this->description = $course['description'];
+      $this->type = $course['type'];
+      $this->category_id = $course['category_id'];
+      $this->teacher_id = $course['teacher_id'];
+      $this->created_at = $course['created_at'];
+      $this->image = $course['image'];
+      $this->text_content = $course['text_content'];
+
+      $this->loadTags($db);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function saveCourse(PDO $db)
+  {
+    $query = "INSERT INTO courses (title, description, type, category_id, teacher_id, created_at, image, text_content)
+    VALUES (:title, :description, :type, :category_id, :teacher_id, :created_at, :image, :text_content)";
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':type', $this->type);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':teacher_id', $this->teacher_id);
+    $stmt->bindParam(':created_at', $this->created_at);
+    $stmt->bindParam(':image', $this->image);
+    $stmt->bindParam(':text_content', $this->text_content);
+
+    if ($stmt->execute()) {
+
+      $this->id = $db->lastInsertId();
+      $this->saveTags($db);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function saveCourseUpdate(PDO $db)
+  {
+    $query = "UPDATE courses SET title = :title, description = :description, category_id = :category_id, image = :image, text_content = :text_content WHERE id = :id";
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':image', $this->image);
+    $stmt->bindParam(':text_content', $this->text_content);
+    $stmt->bindParam(':id', $this->id);
+
+    if ($stmt->execute()) {
+      $this->updateTags($db);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
--- classes/videoCourse.php
+++ classes/videoCourse.php
@@ -3,11 +3,94 @@ require_once 'course.php';
 
 class videoCourse extends Course
 {
-  public function __construct($id, $title, $description, $content, $category_id, $teacher_id, $created_at = null)
+  private $video_path;
+  public function __construct($id, $title = null, $description = null, $teacher_id = null, $image = null, $category_id = null, $tags = null, $video_path = null, $created_at = null)
   {
-    parent::__construct($id, $title, $description, $content, $category_id, $teacher_id, $created_at);
-    $this->type = 'video';
+    parent::__construct($id, $title, $description, $teacher_id, $image, $category_id, $tags, 'video', $created_at);
+    $this->video_path = $video_path;
   }
+  public function getVideoPath()
+  {
+    return $this->video_path;
+  }
+  public function setVideoPath($video_path)
+  {
+    $this->video_path = $video_path;
+  }
+
+  public function loadCourse(PDO $db)
+  {
+    $query = "SELECT * FROM courses WHERE id = :id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $this->id);
+    $stmt->execute();
+
+    $course = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($course) {
+      $this->title = $course['title'];
+      $this->description = $course['description'];
+      $this->type = $course['type'];
+      $this->category_id = $course['category_id'];
+      $this->teacher_id = $course['teacher_id'];
+      $this->created_at = $course['created_at'];
+      $this->image = $course['image'];
+      $this->video_path = $course['video_path'];
+
+      $this->loadTags($db);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function saveCourse(PDO $db)
+  {
+    $query = "INSERT INTO courses (title, description, type, category_id, teacher_id, created_at, image, video_path) 
+              VALUES (:title, :description, :type, :category_id, :teacher_id, :created_at, :image, :video_path)";
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':type', $this->type);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':teacher_id', $this->teacher_id);
+    $stmt->bindParam(':created_at', $this->created_at);
+    $stmt->bindParam(':image', $this->image);
+    $stmt->bindParam(':video_path', $this->video_path);
+
+    if ($stmt->execute()) {
+
+      $this->id = $db->lastInsertId();
+      $this->saveTags($db);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function saveCourseUpdate(PDO $db)
+  {
+    $query = "UPDATE courses SET title = :title, description = :description, category_id = :category_id, image = :image, video_path = :video_path WHERE id = :id";
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':image', $this->image);
+    $stmt->bindParam(':video_path', $this->video_path);
+    $stmt->bindParam(':id', $this->id);
+
+    if ($stmt->execute()) {
+      $this->updateTags($db);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
 }
 
 ?>
