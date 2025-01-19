613f32db990fe7b91e05b9af0b55c1425f3b3ae3
MohamedHamouch <med10hamouch@gmail.com>
2025-01-19 01:14:14 +0100

    logic for delting and unsuspending users from admin dashboard

---
+++ pages/admin/process/deleteUser.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require_once '../../../config/database.php';
+require_once '../../../classes/Admin.php';
+
+if (!isset($_SESSION['user'])) {
+  header('Location: ../../auth/login.php');
+  exit();
+} else {
+  $user = unserialize($_SESSION['user']);
+  if (!($user instanceof Admin)) {
+    header('Location: ../../auth/login.php');
+    exit();
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  if (isset($_POST['userId'])) {
+    $userId = $_POST['userId'];
+    $status = $user->deleteUser($PDOConn, $userId);
+    if ($status) {
+
+      $_SESSION['adminActionSuccess'] = 'User deleted successfully';
+      header('Location: ../adminDashboard.php');
+      exit();
+    } else {
+      $_SESSION['adminActionError'] = 'User deletion failed';
+      header('Location: ../adminDashboard.php');
+      exit();
+    }
+  }
+} else {
+  header('Location: ../adminDashboard.php');
+  exit();
+}
+
+?>
