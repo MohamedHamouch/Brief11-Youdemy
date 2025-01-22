bf0210a62fba5344f224e2904159f618c1d510d3
MohamedHamouch <med10hamouch@gmail.com>
2025-01-22 11:27:17 +0100

    remove connection messages

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $dsn = "mysql:host:$host;port=3306;dbname=$dbName;charset=utf8";
 try {
 
     $PDOConn = new PDO($dsn, $user, $pass);
-    $PDOConn->setAttribute(attribute: PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $PDOConn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $err) {
     echo "Connection error :" . $err->getMessage();
 }
